6b3db148b307c1c8d9013530c6894d0d799ee025
සිතුම් හදපාන්ගොඩ <hsithum98@yahoo.com>
2021-10-10 08:35:43 +0530

    Add files via upload

---
+++ GUI/src/lexer.java
@@ -0,0 +1,163 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class lexer {
+    private final StringBuilder input = new StringBuilder();
+    private Token token;
+    private String lexeme;
+    private boolean exhausted;
+
+    {
+        exhausted = false;
+    }
+
+    private String errorMessage = "";
+    private final Set<Character> blankChars = new HashSet<>();
+
+    public lexer(String filePath) {
+        try (Stream<String> st = Files.lines(Paths.get(filePath))) {
+            st.forEach(input::append);
+        } catch (IOException ex) {
+            exhausted = true;
+            errorMessage = "Could not read file: " + filePath;
+            return;
+        }
+
+        blankChars.add('\r');
+        blankChars.add('\n');
+        blankChars.add((char) 8);
+        blankChars.add((char) 9);
+        blankChars.add((char) 11);
+        blankChars.add((char) 12);
+        blankChars.add((char) 32);
+
+        moveAhead();
+    }
+
+    public void moveAhead() {
+        if (exhausted) {
+            return;
+        }
+
+        if (input.length() == 0) {
+            exhausted = true;
+            return;
+        }
+
+        ignoreWhiteSpaces();
+
+        if (findNextToken()) {
+            return;
+        }
+
+        exhausted = true;
+
+        if (input.length() > 0) {
+            errorMessage = "Unexpected symbol: '" + input.charAt(0) + "'";
+        }
+    }
+
+    private void ignoreWhiteSpaces() {
+        int charsToDelete = 0;
+
+        while (blankChars.contains(input.charAt(charsToDelete))) {
+            charsToDelete++;
+        }
+
+        if (charsToDelete > 0) {
+            input.delete(0, charsToDelete);
+        }
+    }
+
+    private boolean findNextToken() {
+        for (Token t : Token.values()) {
+            int end = t.endOfMatch(input.toString());
+
+            if (end != -1) {
+                token = t;
+                lexeme = input.substring(0, end);
+                input.delete(0, end);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Token currentToken() {
+        return token;
+    }
+
+    public String currentLexeme() {
+        return lexeme;
+    }
+
+    public boolean isSuccessful() {
+        return errorMessage.isEmpty();
+    }
+
+    public String errorMessage() {
+        return errorMessage;
+    }
+
+    public boolean isExhausted() {
+        return exhausted;
+    }
+}
+
+ enum Token {
+    TK_MINUS ("-"),
+    TK_PLUS ("\\+"),
+    TK_MUL ("\\*"),
+    TK_DIV ("/"),
+    TK_NOT ("~"),
+    TK_AND ("&"),
+    TK_OR ("\\|"),
+    TK_LESS ("<"),
+    TK_LEG ("<="),
+    TK_GT (">"),
+    TK_GEQ (">="),
+    TK_EQ ("=="),
+    TK_ASSIGN ("="),
+    TK_OPEN ("\\("),
+    TK_CLOSE ("\\)"),
+    TK_SEMI (";"),
+    TK_COMMA (","),
+    TK_KEY_DEFINE ("define"),
+    TK_KEY_AS ("as"),
+    TK_KEY_IS ("is"),
+    TK_KEY_IF ("if"),
+    TK_KEY_THEN ("then"),
+    TK_KEY_ELSE ("else"),
+    TK_KEY_ENDIF ("endif"),
+    OPEN_BRACKET ("\\{"),
+    CLOSE_BRACKET ("\\}"),
+    DIFFERENT ("<>"),
+
+    STRING ("\"[^\"]+\""),
+    INTEGER ("\\d+"),
+    REAL ("(\\d*)\\.\\d+"),
+    IDENTIFIER ("\\w+");
+
+
+    private final Pattern pattern;
+
+    Token(String regex) {
+        pattern = Pattern.compile("^" + regex);
+    }
+
+    int endOfMatch(String s) {
+        Matcher m = pattern.matcher(s);
+
+        if (m.find()) {
+            return m.end();
+        }
+        return -1;
+    }
+}
